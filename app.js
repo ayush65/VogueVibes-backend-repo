a9123c567638696982ce2706f7c30c94bbf370c2
Ayush Prakash~ <payush381@gmail.com>
2023-04-05 02:37:52 +0530

    refracted js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "https://vogue-vibes-frontend-repo.vercel.app",
     credentials: true,
   })
 );
